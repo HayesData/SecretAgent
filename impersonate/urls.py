c4315294bfd4b61bee23c1e9277d706384691677
Chris Hayes <chris@hayesdata.com>
2013-12-05 03:58:01 -0800

    forking django-impersonate and creating SecretAgent

---
+++ impersonate/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('impersonate.views',
+    url(r'^$', 'list_users',{'template': 'impersonate/list_users.html'}, name='impersonate-list'),
+    url(r'^(?P<uid>\d+)/$', 'impersonate', name='impersonate-start'),
+    url(r'^stop/$', 'stop_impersonate', name='impersonate-stop'),
+    url(r'^search/$', 'search_users',{'template': 'impersonate/search_users.html'}, name='impersonate-search'),
+)
